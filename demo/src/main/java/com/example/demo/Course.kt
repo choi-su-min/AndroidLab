d55a544bf224968863311036868398b6fe41e2cd
choi-su-min <80188948+choi-su-min@users.noreply.github.com>
2022-11-24 14:18:33 +0900

    Delete demo directory

--- demo/src/main/java/com/example/demo/Course.kt
+++
@@ -1,5 +0,0 @@
-package com.example.demo
-
-class Course : ArrayList<CourseItem>(){
-
-}
--- demo/src/main/java/com/example/demo/CourseItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.demo
-
-data class CourseItem(
-    val Course_NAME: String,
-    val Course_POINT: String,
-    val goun_de: String,
-    val sep_rate: String,
-    val spec_num: String
-)
